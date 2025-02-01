41d7035f1170ab46f6ee98bc69ad7b5e1de7a79d
Ahraman <ahraman.programming@gmail.com>
2025-02-01 14:22:20 +0330

    Added task to clean up user_sessions_table every month starting at server startup.

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -1,9 +1,11 @@
+mod db;
+
 use std::sync::Arc;
 
 use tokio::task::JoinHandle;
 
 use crate::App;
 
-pub fn spawn_tasks(_: Arc<App>) -> Vec<JoinHandle<()>> {
-    vec![]
+pub fn spawn_tasks(app: Arc<App>) -> Vec<JoinHandle<()>> {
+    vec![tokio::spawn(db::cleanup_table_user_sessions(app.clone()))]
 }
