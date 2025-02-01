2194a7640a868c87deac0a48d6e4bbc0f073e683
Ahraman <ahraman.programming@gmail.com>
2025-02-01 10:49:00 +0330

    Added migration create_table_user_sessions.

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,15 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    user_sessions (token) {
+        #[max_length = 24]
+        token -> Varchar,
+        user_id -> Int8,
+        expire_on -> Nullable<Timestamptz>,
+    }
+}
+
+diesel::table! {
     users (id) {
         id -> Int8,
         #[max_length = 255]
@@ -12,3 +21,10 @@ diesel::table! {
         updated_on -> Timestamptz,
     }
 }
+
+diesel::joinable!(user_sessions -> users (user_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    user_sessions,
+    users,
+);
