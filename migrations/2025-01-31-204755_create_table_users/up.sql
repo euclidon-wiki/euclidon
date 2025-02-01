504000fb94a0548b35dd3ab0aa757c2f8ac2043b
Ahraman <ahraman.programming@gmail.com>
2025-02-01 10:11:42 +0330

    Added migration create_table_users.

---
+++ migrations/2025-01-31-204755_create_table_users/up.sql
@@ -0,0 +1,8 @@
+create table users (
+    id          bigserial primary key not null,
+    name        varchar(255) unique not null,
+    email       varchar(320) unique not null,
+    password    bytea not null,
+    created_on  timestamptz not null
+        default now()
+);
---
+++ src/schema.rs
@@ -0,0 +1,13 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    users (id) {
+        id -> Int8,
+        #[max_length = 255]
+        name -> Varchar,
+        #[max_length = 320]
+        email -> Varchar,
+        password -> Bytea,
+        created_on -> Timestamptz,
+    }
+}
