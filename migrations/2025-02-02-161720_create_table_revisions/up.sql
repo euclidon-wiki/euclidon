db5929d3f506a54c3f0420ae994630d58c935539
Ahraman <ahraman.programming@gmail.com>
2025-02-02 19:56:16 +0330

    Fixed indentation.

--- migrations/2025-02-02-161720_create_table_revisions/up.sql
+++ migrations/2025-02-02-161720_create_table_revisions/up.sql
@@ -1,16 +1,16 @@
 create table revisions (
-    id bigserial primary key,
-    parent_id bigint default null
+    id          bigserial primary key,
+    parent_id   bigint default null
         references revisions (id)
             on delete cascade,
-    content_id bigint not null
+    content_id  bigint not null
         references contents (id)
             on delete restrict,
     
-    page_id bigint not null,
-    user_id bigint not null
+    page_id     bigint not null,
+    user_id     bigint not null
         references users (id)
             on delete set default,
-    created_on timestamptz not null
+    created_on  timestamptz not null
         default now()
 );
