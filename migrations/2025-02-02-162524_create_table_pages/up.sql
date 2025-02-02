691c00ed1e01ece0c481e026846c68d12a8a0465
Ahraman <ahraman.programming@gmail.com>
2025-02-03 01:16:47 +0330

    Removed extra space.

--- migrations/2025-02-02-162524_create_table_pages/up.sql
+++ migrations/2025-02-02-162524_create_table_pages/up.sql
@@ -1,7 +1,6 @@
 create table pages (
     id          bigserial primary key,
     title       varchar(255) not null unique,
-
     rev_id      bigint not null
         references revisions (id)
             on delete restrict,
