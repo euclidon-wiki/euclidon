b4c6248d6b9e1ef7852508abfba2e1eba310be8b
Ahraman <ahraman.programming@gmail.com>
2025-02-02 18:25:00 +0330

    Created models required for user manipulation in database.

--- src/model/user.rs
+++ src/model/user.rs
@@ -1,8 +1,13 @@
+use std::sync::RwLock;
+
+use base64::{prelude::BASE64_URL_SAFE, Engine};
 use chrono::{DateTime, Utc};
 use diesel::{
-    connection::LoadConnection, pg::Pg, BoolExpressionMethods, Connection, ExpressionMethods,
-    Insertable, QueryDsl, RunQueryDsl,
+    connection::LoadConnection, pg::Pg, prelude::Queryable, BoolExpressionMethods, Connection,
+    ExpressionMethods, Insertable, OptionalExtension, QueryDsl, RunQueryDsl, Selectable,
 };
+use rand_chacha::ChaCha12Rng;
+use rand_core::{RngCore, SeedableRng};
 
 use crate::{
     auth::Password,
@@ -43,12 +48,10 @@ impl<'a> Signup<'a> {
     where
         C: Connection<Backend = Pg> + LoadConnection,
     {
-        let res = users::table
+        Ok(0 != users::table
             .filter(users::name.eq(self.name).or(users::email.eq(self.email)))
             .count()
-            .get_result::<i64>(conn)?;
-
-        Ok(res != 0)
+            .get_result::<i64>(conn)?)
     }
 
     pub fn insert<C>(self, conn: &mut C) -> Result<bool, Error>
@@ -63,18 +66,126 @@ impl<'a> Signup<'a> {
     }
 }
 
-pub fn cleanup_sessions(db: &Db, now: DateTime<Utc>) -> Result<(), Error> {
+#[derive(Selectable, Queryable)]
+#[diesel(table_name = users)]
+pub struct User {
+    pub id: i64,
+    pub name: String,
+    pub email: String,
+    pub password: Password,
+    pub created_on: DateTime<Utc>,
+    pub updated_on: DateTime<Utc>,
+}
+
+impl User {
+    pub fn from_name<C>(name: &str, conn: &mut C) -> Result<Option<Self>, Error>
+    where
+        C: Connection<Backend = Pg> + LoadConnection,
+    {
+        Ok(users::table
+            .filter(users::name.eq(name))
+            .first(conn)
+            .optional()?)
+    }
+
+    pub fn from_email<C>(email: &str, conn: &mut C) -> Result<Option<Self>, Error>
+    where
+        C: Connection<Backend = Pg> + LoadConnection,
+    {
+        Ok(users::table
+            .filter(users::email.eq(email))
+            .first(conn)
+            .optional()?)
+    }
+
+    pub fn mark_invalid<C>(self, conn: &mut C) -> Result<bool, Error>
+    where
+        C: Connection<Backend = Pg>,
+    {
+        Ok(
+            0 != diesel::update(users::table.filter(users::id.eq(self.id)))
+                .set(users::password.eq(Password::Invalid))
+                .execute(conn)?,
+        )
+    }
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = user_sessions)]
+pub struct Session {
+    pub token: String,
+    pub user_id: i64,
+    pub expire_on: Option<DateTime<Utc>>,
+}
+
+impl Session {
+    pub fn generate<C>(
+        user_id: i64,
+        expire_on: Option<DateTime<Utc>>,
+        conn: &mut C,
+    ) -> Result<Self, Error>
+    where
+        C: Connection<Backend = Pg> + LoadConnection,
+    {
+        Ok(Self {
+            token: Self::new_token(conn)?,
+            user_id,
+            expire_on,
+        })
+    }
+
+    pub fn insert<C>(self, conn: &mut C) -> Result<bool, Error>
+    where
+        C: Connection<Backend = Pg> + LoadConnection,
+    {
+        Ok(0 != self
+            .insert_into(user_sessions::table)
+            .on_conflict_do_nothing()
+            .execute(conn)?)
+    }
+
+    fn new_token<C>(conn: &mut C) -> Result<String, Error>
+    where
+        C: Connection<Backend = Pg> + LoadConnection,
+    {
+        thread_local! {
+        static TOKEN_RAND: RwLock<ChaCha12Rng> = RwLock::new(ChaCha12Rng::from_os_rng());
+        }
+
+        Ok(loop {
+            let mut buf = [0; 16];
+            TOKEN_RAND.with(|rand| rand.write().expect("RWLock poisoned").fill_bytes(&mut buf));
+            let token = BASE64_URL_SAFE.encode(buf);
+
+            if Self::exists(&token, conn)? {
+                continue;
+            } else {
+                break token;
+            }
+        })
+    }
+
+    fn exists<C>(token: &str, conn: &mut C) -> Result<bool, Error>
+    where
+        C: Connection<Backend = Pg> + LoadConnection,
+    {
+        Ok(0 != user_sessions::table
+            .filter(user_sessions::token.eq(token))
+            .count()
+            .get_result::<i64>(conn)?)
+    }
+}
+
+pub fn cleanup_sessions(db: &Db, now: DateTime<Utc>) -> Result<bool, Error> {
     let mut conn = db.pool.get()?;
-    diesel::delete(
+    Ok(0 != diesel::delete(
         user_sessions::table.filter(
             user_sessions::expire_on
                 .is_not_null()
                 .and(user_sessions::expire_on.lt(now)),
         ),
     )
-    .execute(&mut conn)?;
-
-    Ok(())
+    .execute(&mut conn)?)
 }
 
 #[cfg(test)]
